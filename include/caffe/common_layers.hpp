d8e4b2b72197470a5ccfe4438a21ad193b264a75
NicolaiHarich <nicolai.harich@gmail.com>
2015-10-21 14:15:44 +0200

    ExactNumBottomBlobs

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -784,7 +784,7 @@ class VoidThresholdLayer : public Layer<Dtype> {
       const vector<Blob<Dtype>*>& top);
 
   virtual inline const char* type() const { return "VoidThreshold"; }
-  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
   virtual inline int ExactNumTopBlobs() const { return 1; }
 
  protected:
