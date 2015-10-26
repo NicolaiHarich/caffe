a1aad9ac2c73bfa64107f7e3d99987c6ed115144
NicolaiHarich <nicolai.harich@gmail.com>
2015-10-26 13:38:15 +0100

    backprob

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -453,11 +453,8 @@ class FirstSecRatioLayer : public Layer<Dtype> {
    */
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  /// @brief Not implemented (non-differentiable function)
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    NOT_IMPLEMENTED;
-  }
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 };
 
 /**
