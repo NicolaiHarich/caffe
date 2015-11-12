1f3aa7bd9eb1b084dd93a71e5207c2c9449b66d3
NicolaiHarich <nicolai.harich@gmail.com>
2015-11-12 11:13:59 +0100

    remove gpu-methods

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -878,11 +878,8 @@ class SoftmaxWithLoss2GtLayer : public LossLayer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
   /// The internal SoftmaxLayer used to map predictions to a distribution.
